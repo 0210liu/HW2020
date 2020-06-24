1398d49bd33cb73733fc6804cb61b064844fa109
0210liu <62094607+0210liu@users.noreply.github.com>
2020-06-24 16:57:07 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,19 +1,18 @@
+let tasks = []; // {title:"ddddd",done:false}
 
- let task =[];//{title:"dddddd",done:false}
+function renderEditor() {
+    let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
 
- function renderEditor(){
-     let inputEl = document.querySelector("#default-todo-panel.todo-editor>input");
+    //inputEl.onchange = (e) => {
+    //  console.log("text:", e.target.value);
+    //    console.log("input change:", e);
+    // };
 
-     //inputEl.onchange = (e)=> {
-     //    console.log("text,",e.target.value);
-     //    //console.log("input change: ",e);
-     // };
+    let addTask = () => {
+        if (inputEl.value.length === 0) {
+            return;
+        }
 
-     let addTask = () => {
-         if(inputEl.value.length===0) {
-             return;
-         }
-         
         let newTask = {
             title: inputEl.value,
             done: false,
@@ -23,71 +22,53 @@
 
         tasks.push(newTask);
 
-        console.log("tasks: ",tasks);
+        console.log("task: ", tasks);
 
-        renderTaskItems(); 
-     }
+        renderTaskItems();
+    };
 
-     inputEl.onkeypress = (e) =>  {
-        
-         if(e.key==="Enter") {
+    inputEl.onkeypress = (e) => {
+
+        if (e.key === "Enter") {
             addTask();
-         }
-     };
+        }
+    };
 
-     let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
-     addEl.onclick = (e) => {
-         addTask();
-     };
+    let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
+    addEl.onclick = (e) => {
+        addTask();
+    };
+
 }
 
 function renderTaskItems() {
     console.log("render items");
     let itemsEl = document.querySelector("#default-todo-panel .todo-items");
 
-    itemsEl.querySelectorAll("div").forEach((node)=>node.remove());
+    itemsEl.querySelectorAll("div").forEach((node) => node.remove());
+    console.log(itemsEl);
 
-    for (let i = 0; i < tasks.length; i++ ) {
+    for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
         let itemEl = document.createElement("div");
+        itemEl.className = "task";
 
         let doneEl = document.createElement("input");
         doneEl.type = "checkbox";
-        doneEl.onchange = (e) => {
-            console.log("checkbox: ",e);           
-        }
-        itemEl.append(doneEl);
-
-        let titleEl = document.createElement("label");
-        titleEl.innerText = task.title;
-        itemEl.append(titleEl);
-
-        let cancelEl = document.createElement("button");
-        cancelEl.innerText = "X";
-        cancelEl.onclick = () => {
-            tasks.splice(i, 1);  
-            renderTaskItems();
-        };
-
-        itemEl.append(cancelEl);
-
-        itemsEl.append(itemEl);
-    }
-}
-
-renderEditor();
-renderTaskItems()
-        if(task.done) {
+        doneEl.checked = task.done;
+        if (task.done) {
             itemEl.classList.add("done");
-        }else{
+        }
+        else {
             itemEl.classList.remove("done");
         }
 
         doneEl.onchange = (e) => {
             task.done = e.target.checked;
-            if(task.done) {
+            if (task.done) {
                 itemEl.classList.add("done");
-            } else {
+            }
+            else {
                 itemEl.classList.remove("done");
             }
         }
@@ -97,7 +78,7 @@ renderTaskItems()
         titleEl.innerText = task.title;
         itemEl.append(titleEl);
 
-        let ctrlbarEl = renderTaskCtrlBar(task, i);
+        let ctrlbarEl = renderTaskCtrlBar(tasks, i);
 
         itemEl.append(ctrlbarEl);
 
@@ -106,36 +87,53 @@ renderTaskItems()
 }
 
 
-function renderTaskCtrlBar(tasks,taskIdx) {
-        let ctrlvarEl = document.createElement("div");
-        ctrlbarEl.className ="ctrlbar";
+function renderTaskCtrlBar(tasks, taskIdx) {
+    let ctrlbarEl = document.createElement("div");
+    ctrlbarEl.className = "ctrlbar";
 
-       let upEl = document.createElement("button");
-       upEl.innerText = "1";
-       upEl.onclick = () =>{
-           //
-       };
-       ctrlbarEl.append(upEl);
+    let impEl = document.createElement("button");
+    var div = document.getElementsByTagName('impEl')[0];
+    impEl.innerText = "⊿";
+    var count = 0;
+    impEl.onclick = function (a) {
+        count++
+        if (count % 2 === 1) {
+            impEl.innerText = "重要";
+        } else {
+            impEl.innerText = "⊿";
+        }
+    }
 
-       let downEl = document.createElement("button");
-        downEl.innerText = "↓";
-        downEl.onclick = () => {
-            //
-        };
-        ctrlbarEl.append(downEl);
+    ctrlbarEl.append(impEl);
 
-
-        let cancelEl = document.createElement("button");
-        cancelEl.innerText = "X";
-        cancelEl.onclick = () => {
-            tasks.splice(taskIdx, 1);
-            renderTaskItems();
-        };
-
-        ctrlbarEl.append(cancelEl);
-
-        return ctrlbarEl;
+    let upEl = document.createElement("button");
+    if (taskIdx === 0) {
+        upEl.disabled = true;
     }
-          
+    upEl.innerText = "↑";
+    upEl.onclick = () => {
+        //
+    }
+    ctrlbarEl.append(upEl);
+
+    let downEl = document.createElement("button");
+    downEl.innerText = "↓";
+    downEl.onclick = () => {
+        //
+    };
+    ctrlbarEl.append(downEl);
+
+    let cancelEl = document.createElement("button");
+    cancelEl.innerText = "X";
+    cancelEl.onclick = () => {
+        tasks.splice(taskIdx, 1);
+        renderTaskItems();
+    };
+
+    ctrlbarEl.append(cancelEl);
+
+    return ctrlbarEl;
+}
+
 renderEditor();
 renderTaskItems();
